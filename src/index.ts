3f832d1ebbd184a6400e43c1b47a4fb6c7f6d6a7
Lindsay Wardell <three060@gmail.com>
2020-05-11 09:34:50 -0700

    Enabled syncing between clients

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ import socketIo from "socket.io";
 
 const port = process.env.PORT || 5000;
 
+const LOBBY = "lobby";
+
+let connections: string[] = [];
+
+// TODO - type the game.
+// Connecting players will either receive the gamestate, or be requested to provide it
+let games: any[] = [];
+
 log()
   // .then(() => connectToDb())
   .then(() => {
@@ -19,6 +27,72 @@ log()
     const io = socketIo(http);
 
     io.on("connection", (socket) => {
-      console.log("Client connected!");
+      console.log("Client connected!", socket.id);
+
+      connections = [...connections, socket.id];
+
+      socket.join(LOBBY);
+
+      io.to(LOBBY).emit("update-game-list", games);
+
+      socket.on("start-game", () => {
+        console.log("The game is starting");
+        const id = Math.floor(Math.random() * 999999999);
+
+        console.log(id);
+
+        games = [...games, { id, connections: [socket.id], state: null }];
+
+        io.to(LOBBY).emit("update-game-list", games);
+        
+        socket.join(id.toString());
+
+        socket.emit("enter-game", id);
+      });
+
+      socket.on("enter-game", (id: number) => {
+        console.log("Someone is joining a game", id);
+        socket.join(id.toString());
+
+        games = games.map((game) =>
+          game.id === id
+            ? {
+                ...game,
+                connections: [...new Set([...game.connections, socket.id])],
+              }
+            : game
+        );
+
+        io.to(LOBBY).emit("update-game-list", games);
+
+        socket.emit("enter-game", id);
+      });
+
+      socket.on("entered", (id: number) => {
+        const game = games.find((g) => g.id === id);
+        socket.emit("sync", game?.state);
+      });
+
+      socket.on("sync", ({ id, state }: { id: number; state: any }) => {
+        if (id) {
+          games = games.map((game) =>
+            game.id === id ? { ...game, state } : game
+          );
+          io.to(id.toString()).emit("sync", state);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        connections = connections.filter((conn) => conn === socket.id);
+
+        games = games
+          .map((game) => ({
+            ...game,
+            connections: game.connections.filter(
+              (conn: string) => conn !== socket.id
+            ),
+          }))
+          .filter((game) => game.connections.length > 0);
+      });
     });
   });
